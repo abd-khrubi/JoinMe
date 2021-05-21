a442d433e9f3251adf8f3afcb4a8ed4a7ab780a9
KareemAbbasi <kareem.abbasi@mail.huji.ac.il>
2021-05-21 04:29:25 +0300

    Upload request to database

--- lib/widgets/path_list_item.dart
+++ lib/widgets/path_list_item.dart
@@ -1,13 +1,34 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
 import 'package:flutter_app/models/activity_path.dart';
 import 'package:flutter_app/models/activity.dart';
+import 'package:flutter_app/models/request.dart';
 import 'package:flutter_app/screens/google_map_screen.dart';
+import 'package:flutter_app/services/request_service.dart';
+import 'package:flutter_app/utils/firebase_utils.dart';
+import 'package:uuid/uuid.dart';
 
 class PathListItem extends StatelessWidget {
   late final ActivityPath path;
 
   PathListItem(this.path);
+
+  _upload(BuildContext context) {
+    var uuid = Uuid();
+    var randUid = uuid.v1();
+    var user = getCurrentUser();
+    var date = DateTime.now();
+    Request r = Request(
+      randUid, user.uid, path.pathActivity, path.pathCampus, date, date);
+    saveRequest(r).then((value) {
+      print('saved requested');
+      int count = 0;
+      Navigator.of(context).popUntil((_) => count++ >= 3);
+    }).catchError((error) {
+      print(error);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,32 +55,38 @@ class PathListItem extends StatelessWidget {
             activityImagePath(path.pathActivity),
             height: 50,
           ),
-          Container(
-            padding: EdgeInsets.only(left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AutoSizeText(
-                  path.pathName,
-                  style: TextStyle(fontSize: 18),
-                  maxLines: 2,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("${path.pathLength.toString()} KM"),
-                // Text(
-                //   path.pathName,
-                //   style: TextStyle(fontSize: 26),
-                // ),
-              ],
+          GestureDetector(
+            onTap: () {
+              _upload(context);
+            },
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    path.pathName,
+                    style: TextStyle(fontSize: 18),
+                    maxLines: 2,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text("${path.pathLength.toString()} KM"),
+                  // Text(
+                  //   path.pathName,
+                  //   style: TextStyle(fontSize: 26),
+                  // ),
+                ],
+              ),
             ),
           ),
           Container(
             margin: EdgeInsets.only(left: 15),
             child: IconButton(
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => GoogleMapScreen()));
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => GoogleMapScreen()));
               },
               icon: Icon(
                 Icons.directions,
